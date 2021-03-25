830709ba9b2b0546e1a39fb9b5fc286f72ee1e51
Vinith Johnson <20274335+razor23@users.noreply.github.com>
2021-03-24 22:14:09 -0700

    Create Step_Burst_Thresholds_minute_by_minute.m

---
+++ Scripts/Step_Burst_Thresholds_minute_by_minute.m
@@ -0,0 +1,90 @@
+%% Beta Burst %%
+
+clear all; close all; clc
+
+% Load up the data folder with all the files in %
+
+InDir = '/Volumes/Vinith2/Starr/Dystonia/RawData';
+Groups = {'RCS04L','RCS04R'};
+Devices = {'DeviceNPC700418H','DeviceNPC700412H'};
+
+%% Load file
+
+for G=1:length(Groups)
+    load(sprintf('%s/%s/StimSess_gamma_Dec5.mat',InDir,Groups{G}));
+    ONfiles  = StimSess(:,1);
+    OFFfiles = StimSess(:,2);
+    ONfiles =  ONfiles(~cellfun('isempty',ONfiles)); % there are some empty cells, geting rid of em
+    OFFfiles = OFFfiles(~cellfun('isempty',OFFfiles));
+    thrsh1{G}=[];thrsh2{G}=[];thrsh3{G}=[];
+    
+    fprintf('\n Printing OFF files \n');
+    
+    for f=1:length(OFFfiles)
+        try
+            FlName = OFFfiles{f};
+            fprintf('\n Loading file %s from %s',FlName,Groups{G});
+            path = sprintf('%s/%s/%s/%s',InDir,Groups{G},FlName,Devices{G});
+            Infile = sprintf('%s/%s/%s/%s/processed_data_gamma_Dec5.mat',InDir,Groups{G},FlName,Devices{G});
+            load(Infile);
+            
+            %% Plot the theta range signal
+            
+            s=1;
+            [b,a] = butter(4,[3,7]/(250/2)); % 4th order butterworth filter
+            
+            for u=1:floor(size(Data.temp0,1)/(60*250))
+                
+            l= 250*60*u;
+                                          
+            temp1outTheta=filtfilt(b,a,Data.temp0(s:l));
+            temp2outTheta=filtfilt(b,a,Data.temp2(s:l));
+            temp3outTheta=filtfilt(b,a,Data.temp3(s:l));
+            
+            t1amp=abs(hilbert(temp1outTheta));
+            t2amp=abs(hilbert(temp2outTheta));
+            t3amp=abs(hilbert(temp3outTheta));
+            
+            Thresh1=prctile(t1amp,75);  % gets value of 75th percentile
+            Thresh2=prctile(t2amp,75);  % gets value of 75th percentile
+            Thresh3=prctile(t3amp,75);  % gets value of 75th percentile
+            
+            thrsh1{G} = cat(1,thrsh1{G},Thresh1);
+            thrsh2{G} = cat(1,thrsh2{G},Thresh2);
+            thrsh3{G} = cat(1,thrsh3{G},Thresh3);
+            
+            s=l+1;
+            
+            end
+            
+            clear t1amp t2amp t3amp
+           
+        catch
+             fprintf('\n error \n');
+             clear t1amp t2amp t3amp
+           continue
+        end
+    end
+end
+
+clear Data
+%% Get the threshold values
+
+T1=mean(thrsh1{1}(:,:)); %75th Percentile value OFF files LEFT STN1
+T2=mean(thrsh2{1}(:,:)); %75th Percentile value OFF files LEFT M1
+T3=mean(thrsh3{1}(:,:)); %75th Percentile value OFF files LEFT M2
+T4=mean(thrsh1{2}(:,:)); %75th Percentile value OFF files RIGHT STN1
+T5=mean(thrsh2{2}(:,:)); %75th Percentile value OFF files RIGHT M1
+T6=mean(thrsh3{2}(:,:)); %75th Percentile value OFF files RIGH  M2
+
+
+
+
+
+
+
+
+
+
+
+
